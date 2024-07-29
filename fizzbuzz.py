e07f53752620cd55129e315666287379b3995b02
johanhobbs <139718130+johanhobbs@users.noreply.github.com>
2024-07-29 15:58:44 +1200

    rephrased comments

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,4 +1,4 @@
 # Run through the numbers between 1 and 100 and print the number, BUT
-# For each number that is a multiple of 3, print Fizz
-# For each number that is a multiple of 5, print Buzz
-# For each number that is a multiple of both, print FizzBuzz
+# For multiples of 3, print Fizz
+# For multiples of 5, print Buzz
+# For multiples of both, print FizzBuzz
